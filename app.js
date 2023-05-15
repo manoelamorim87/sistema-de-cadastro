5a6e1ffbe155fa6561d8847ae732c3f4420660d3
Manoel Amorim <manoelamorim87@gmail.com>
2023-05-15 17:15:49 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicação está se conectando ao banco de dados")
-//Mais um console log de teste
-console.log("Enviando para o GitHub");
